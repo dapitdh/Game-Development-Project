a2fb41e572ab6ee88ed965aee25d9a9f99edcbe2
dapitdh <dapitharianto1912@gmail.com>
2025-09-18 15:00:39 +0700

    Add Basic Interaction Playe: Open/Close Door

---
+++ Assets/Prefabs/Pintu/Script/CameraOpenDoor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CameraDoorScript
+{
+public class CameraOpenDoor : MonoBehaviour {
+	public float DistanceOpen=3;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RaycastHit hit;
+		if (Physics.Raycast (transform.position, transform.forward, out hit, DistanceOpen)) {
+				if (hit.transform.GetComponent<DoorScript.Door> ()) {
+					if (Input.GetKeyDown(KeyCode.Mouse0))
+						hit.transform.GetComponent<DoorScript.Door> ().OpenDoor();
+				}
+			}
+		}
+	}
+}
